d2c1b43129647acfc7b6007d46b4124081db5c4e
chandMessi10 <surajchand3010@gmail.com>
2023-04-03 11:33:09 +0545

    change in jenkinsfile

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:mobile_devops/firebase_options.dart';
 
 Future<void> main() async{
+  runApp(const MyApp());
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
